672c3833696833a43e71ee93ee3e77b9590a62ea
billaheagle <mutashimb7@gmail.com>
2023-06-21 16:36:36 +0700

    add handler for cmd

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ if (process.argv[2] === undefined) {
     console.log("Harap masukan File");
 } else {
     var file = process.argv[2];
-    var excelFile = file.substring(1);
 
-    const ext = excelFile.split(".");
+    if (file.charAt(0) == ".") file = file.substring(1);
+    if (file.charAt(0) != "\\") file = "\\" + file;
+
+    const ext = file.split(".");
     if (ext[1] == "xlsx" || ext[1] == "xls") {
-        run.generateCSV(excelFile);
+        run.generateCSV(file);
     } else {
         console.log("Harap masukan file Excel");
     }
